5698c76d747e00656e2b487489617dc4c0e1771b
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-09 23:46:05 +0300

    Bonus

---
+++ index.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+
+describe("Sample Test Suite", () => {
+  describe("Basic Functionality", () => {
+    it("should return true", () => {
+      expect(true).to.be.true;
+    });
+  });
+});
